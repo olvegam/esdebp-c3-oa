5ac9ec4899b3e47dfaffd4430c3613a4c84de157
Olga Lucero Vega Márquez <olvegam@unillanos.edu.co>
2019-05-29 22:29:10 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
-function verMas() {
-	document.getElementById('verMenos').style.display = 'block';
-	document.getElementById('verMas').style.display = 'none';
-	var completText = document.getElementsByClassName("completText");
-	for (var i=0; i<completText.length; i++) {
-		completText[i].style.display = 'block';
-	}
-	document.getElementByClassName("listCond").style.display = 'block';
+function verMas(textNum) {
+	document.getElementById('verMenos'+textNum).style.display = 'block';
+	document.getElementById('verMas'+textNum).style.display = 'none';
+	var completText = document.getElementsByClassName("completText"+textNum);
+	completText[0].style.display = 'block';
+	//document.getElementsByClassName("listCond").style.display = 'block';
 }
 
-function verMenos() {
-	document.getElementById('verMas').style.display = 'block';
-	document.getElementById('verMenos').style.display = 'none';
-	var completText = document.getElementsByClassName("completText");
-	for (var i=0; i<completText.length; i++) {
-		completText[i].style.display = 'none';
-	}
-	document.getElementByClassName("listCond").style.display = 'none';
+function verMenos(textNum) {
+	document.getElementById('verMas'+textNum).style.display = 'block';
+	document.getElementById('verMenos'+textNum).style.display = 'none';
+	var completText = document.getElementsByClassName("completText"+textNum);
+	completText[0].style.display = 'none';
+	//document.getElementsByClassName("listCond").style.display = 'none';
 }
